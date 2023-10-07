ec60e3a218b9d63a79a2644cd901903123b3ea59
McKotMaria <146555091+McKotMaria@users.noreply.github.com>
2023-10-07 15:20:14 +0300

    Add files via upload
     ad a first version

---
+++ ping_pong.py
@@ -0,0 +1,52 @@
+from pygame import *
+
+back=(200, 255, 255)
+win_height = 500
+win_width = 600
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < win_height- 80:
+            self.rect.y += self.speed
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+
+    display.update()
+    clock.tick(FPS)
+    
+  
